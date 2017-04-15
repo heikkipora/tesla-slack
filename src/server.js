11bd46f92b432f48b25f5725f2805cfb2f552b8b
Heikki Pora <heikki.pora@reaktor.fi>
2017-04-15 20:33:50 +0300

    Rewrite using async/await and tesla-api library. Simplified operation and support for both Model S and X

--- src/server.js
+++ src/server.js
@@ -2,53 +2,50 @@ if (process.env.NODE_ENV == 'production') {
   require('newrelic')
 }
 
-const Bacon = require('baconjs').Bacon
+const _ = require('lodash')
 const bodyParser = require('body-parser')
 const express = require('express')
 const logfmt = require('logfmt')
-const tesla = require('./tesla')
+const {listVehicles, vehicleInfo, vehicleWakeUp} = require('./tesla')
 
 const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(logfmt.requestLogger())
 
-app.post('/slack', (req, res) => {
+app.post('/slack', async (req, res) => {
   if (req.body.token != process.env.SLACK_RECEIVE_TOKEN) {
     return res.sendStatus(403)
   }
 
   const args = req.body.text.trim().split(' ')
-  const command = args.length > 1 ? args[1] : undefined
-  const name = 'tessi'
+  const name = args.length > 1 ? args[1] : ''
 
-  if (command === 'battery') {
-    tesla.chargeState(name).mapError(errorText).map(toSlackMessage).onValue(sendJson(res))
-  } else if (command === 'climate') {
-    tesla.climateState(name).mapError(errorText).map(toSlackMessage).onValue(sendJson(res))
-  } else if (command === 'position') {
-    tesla.formattedDriveState(name).mapError(errorText).map(toSlackMessage).onValue(sendJson(res))
-  } else if (command === 'vehicle') {
-    tesla.vehicleState(name).mapError(errorText).map(toSlackMessage).onValue(sendJson(res))
-  } else {
-    res.json(toSlackMessage('Supported commands: battery, climate, position, vehicle.'))
+  try {
+    const vehicles = await listVehicles(process.env.TESLA_USERNAME, process.env.TESLA_PASSWORD)
+    const vehicleNames = _.map(vehicles, 'displayName')
+    const vehicle = _.find(vehicles, vehicle => vehicle.displayName.toLowerCase() == name.toLowerCase())
+
+    if (vehicle) {
+      const infoLines = await vehicleInfo(vehicle)
+      const response = [`Vehicle: ${vehicle.displayName}`].concat(infoLines).join('\n')
+      res.json(toSlackMessage(response))
+    } else {
+      res.json(toSlackMessage(`Specify one of the vehicle names: ${vehicleNames.join(', ')}`))
+    }
+  } catch (err) {
+    console.error(err)
+    res.status(400).json(err)
   }
 })
 
 app.get('/', (req,res) => {
-  res.send('ok') // used for newrelic monitoring at Heroku
+  // used for newrelic monitoring at Heroku
+  res.send('ok')
 })
 
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Listening on *:${port}`))
 
-function errorText(error) {
-  return JSON.stringify(error)
-}
-
 function toSlackMessage(text) {
-  return { text: text,  username: process.env.SLACK_BOT_NAME || 'Tesla Model S' }
-}
-
-function sendJson(res) {
-  return message => res.json(message)
+  return { text: text,  username: process.env.SLACK_BOT_NAME || 'Tesla Bot' }
 }
--- src/tesla.js
+++ src/tesla.js
@@ -1,192 +1,72 @@
-var TESLA_CREDENTIALS = {email: process.env.TESLA_USERNAME, password: process.env.TESLA_PASSWORD };
-var Bacon = require('baconjs').Bacon,
-    teslams = require('teslams'),
-    _ = require('lodash'),
-    geolib = require('geolib'),
-    knownPlaces = require('./known-places');
+const  _ = require('lodash')
+const geolib = require('geolib')
+const knownPlaces = require('./known-places')
+const Tesla = require('tesla-api')
 
-var cachedVehicles = [];
-fetchVehicles().log()
+async function listVehicles(email, password) {
+  return await Tesla.login({email, password, distanceUnit: 'km'})
+}
 
-function milesToKm(value) {
-    return value * 1.60934;
+async function vehicleWakeUp(vehicle) {
+  await vehicle.wakeUp()
+}
+
+async function vehicleInfo(vehicle) {
+  const battery = await vehicleBattery(vehicle)
+  const speedAndLocation = await vehicleSpeedAndLocation(vehicle)
+  return battery.concat(speedAndLocation)
+}
+
+async function vehicleSpeedAndLocation(vehicle) {
+  const state = await vehicle.driveState()
+  return [`Speed: ${state.speed || 0} km/h`,
+          `Location: ${location(state)}`]
+}
+
+async function vehicleBattery(vehicle) {
+  const state = await vehicle.chargeState()
+  const range = `Current range is ${state.estBatteryRange}-${state.idealBatteryRange} km.`
+  switch (state.chargingState) {
+    case 'Charging':
+      const timeToFull = toHoursAndMinutes(state.timeToFullCharge)
+      return [`Charging at ${state.chargerPower} kW, complete in ${timeToFull}. ${range}`]
+    case 'Complete':
+      return [`Fully charged. ${range}`]
+    default:
+      return [`Disconnected. ${range}`]
+  }
 }
 
 function toHoursAndMinutes(value) {
-    var hours = Math.floor(value);
-    var minutes = Math.round((value - hours) * 60);
-    var msg = '';
+  const hours = Math.floor(value)
+  const minutes = Math.round((value - hours) * 60)
+  let msg = ''
 
-    if (hours > 0) {
-        msg = hours + ' h';
-    }
-    if (minutes > 0) {
-        msg += (hours ? ' ' : '') + minutes + ' min';
-    }
-    return msg;
+  if (hours > 0) {
+    msg = hours + ' h'
+  }
+  if (minutes > 0) {
+    msg += (hours ? ' ' : '') + minutes + ' min'
+  }
+  return msg
 }
 
-function fetchVehicles() {
-    if (cachedVehicles.length > 0) {
-        return Bacon.later(0, cachedVehicles);
-    }
-    return Bacon.fromNodeCallback(function(callback) {
-      teslams.all(TESLA_CREDENTIALS, function(err, response, body) {
-          callback(err, body);
-      })
-    })
-        .map(JSON.parse)
-        .map('.response')
-        .map(function(response) {
-            return response.map(function(vehicle) {
-                return { id: vehicle.id, name: vehicle.display_name.toLowerCase() };
-            })
-        })
-        .doAction(function(vehicleIds) {
-            cachedVehicles = vehicleIds;
-        })
+function location(state) {
+  if (state.latitude && state.longitude) {
+    const inKnownPlace = isInKnownPlace(state.latitude, state.longitude)
+    const position = `${state.latitude},${state.longitude}`
+    const googleMapsUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${position}&markers=${position}&size=600x300&zoom=12`
+    return inKnownPlace ? `${inKnownPlace.name} @ ${googleMapsUrl}` : googleMapsUrl
+  }
+  return 'unknown'
 }
 
-function nameToVehicleId(name) {
-    return fetchVehicles().flatMap(function(vehicles) {
-        var vehicle = _.find(vehicles, {name: name.toLowerCase()})
-        if (vehicle) {
-          return Bacon.once(vehicle.id)
-        } else {
-          return Bacon.once(new Bacon.Error('Unrecognized vehicle name: ' + name))
-        }
-    });
+function isInKnownPlace(latitude, longitude) {
+  return _.find(knownPlaces, place => geolib.getDistance({latitude, longitude}, place) < place.radius)
 }
 
-function fetchChargeState(vehicleId) {
-    return Bacon.fromCallback(teslams.get_charge_state, vehicleId);
+module.exports = {
+  listVehicles,
+  vehicleInfo,
+  vehicleWakeUp
 }
-
-function fetchDriveState(vehicleId) {
-    return Bacon.fromCallback(teslams.get_drive_state, vehicleId).flatMap(teslaErrorToBaconError(vehicleId));
-}
-
-function fetchVehicleState(vehicleId) {
-    return Bacon.fromCallback(teslams.get_vehicle_state, vehicleId);
-}
-
-function fetchClimateState(vehicleId) {
-    return Bacon.fromCallback(teslams.get_climate_state, vehicleId);
-}
-
-function wakeUp(vehicleId) {
-  return Bacon.fromCallback(teslams.wake_up, vehicleId);
-}
-
-function teslaErrorToBaconError(vehicleId) {
-  return function(teslaAPIresponse) {
-    if(teslaAPIresponse instanceof Error) {
-      console.log(teslaAPIresponse)
-      wakeUp(vehicleId).onValue(function(apiResponse) {
-        console.log("Tried to wake up vehicle: ", apiResponse);
-      });
-      return new Bacon.Error(teslaAPIresponse);
-    }
-    else return teslaAPIresponse;
-  };
-}
-
-function mapRange(state) {
-    var estimatedRange = milesToKm(state.est_battery_range).toFixed(0);
-    var idealRange = milesToKm(state.ideal_battery_range).toFixed(0);
-    return 'Current range is ' + estimatedRange + '-' + idealRange + ' km.';
-}
-
-function mapChargeResponse(state) {
-    switch (state.charging_state) {
-        case 'Charging':
-            var timeToFull = toHoursAndMinutes(state.time_to_full_charge);
-            return 'Charging at ' + state.charger_power + ' kW, complete in ' + timeToFull + '. ' + mapRange(state);
-        case 'Complete':
-            return 'Fully charged. ' + mapRange(state);
-        default:
-            return 'Disconnected. ' + mapRange(state);
-    }
-}
-function mapDriveResponse(state) {
-  return  _.assign({}, state,
-    {speed: Math.round(milesToKm(state.speed || 0)),
-     location: [state.longitude, state.latitude],
-     shift_state: state.shift_state ? state.shift_state : "P"
-    });
-}
-
-function formatDriveResponse(state) {
-    var speed = milesToKm(state.speed || 0).toFixed(0);
-    var speedTxt = 'Speed: ' + speed + 'km/h\n';
-    var locationTxt = 'Position: unknown';
-    if (state.latitude && state.longitude) {
-        var inKnownPlace = isInAlreadyKnownPlace(state.latitude, state.longitude);
-        var position = state.latitude + ',' + state.longitude;
-        var googleMapsUrl = 'http://maps.googleapis.com/maps/api/staticmap?center=' + position + '&markers=' + position + '&size=600x300&zoom=12';
-        locationTxt = 'Position: ' + (inKnownPlace ? inKnownPlace.name + ' @ ': '' ) + googleMapsUrl;
-    }
-    return speedTxt + locationTxt;
-}
-
-function isInAlreadyKnownPlace(latitude, longitude) {
-    return _.find(knownPlaces, function(place) {
-      var distance = geolib.getDistance({latitude: latitude, longitude: longitude}, place);
-      return distance < place.radius;
-    });
-}
-
-function mapVehicleResponse(state) {
-    return 'Firmware version: ' + state.car_version + '\nLocked: ' + (state.locked ? 'yes' : 'no');
-}
-
-function mapClimateResponse(state) {
-    var internalTemp = feelsLike(state.inside_temp) + ' (' + state.inside_temp + 'C)';
-    var externalTemp = feelsLike(state.outside_temp) + ' (' + state.outside_temp + 'C)';
-    return 'Inside: ' + internalTemp +'\nOutside: ' + externalTemp;
-}
-
-function feelsLike(temp) {
-    if (temp <= -20) {
-        return "HORRIBLY COLD NUCLEAR WINTER WEATHER";
-    } else if (temp <= -10) {
-        return "darn cold";
-    } else if (temp <= 0) {
-        return "quite crispy";
-    } else if (temp <= 10) {
-        return "a bit nippy";
-    } else if (temp <= 20) {
-        return "cool and mellow";
-    } else if (temp <= 25) {
-        return "nice and balmy";
-    } else if (temp <= 30) {
-        return "warm and toasty";
-    } else {
-        return "hot and sweaty";
-    }
-}
-
-exports.vehicleNames = function() {
-    return fetchVehicles().map(function(vehicles) {
-      return 'Known vehicles: ' + _.map(vehicles, 'name').join(', ')
-    });
-}
-
-exports.chargeState = function(name) {
-    return nameToVehicleId(name).flatMap(fetchChargeState).map(mapChargeResponse);
-};
-
-exports.driveState = function(name) {
-    return nameToVehicleId(name).flatMap(fetchDriveState).map(mapDriveResponse);
-};
-exports.formattedDriveState = function(name) {
-  return nameToVehicleId(name).flatMap(fetchDriveState).map(formatDriveResponse);
-};
-
-exports.vehicleState = function(name) {
-    return nameToVehicleId(name).flatMap(fetchVehicleState).map(mapVehicleResponse);
-};
-
-exports.climateState = function(name) {
-    return nameToVehicleId(name).flatMap(fetchClimateState).map(mapClimateResponse);
-};
